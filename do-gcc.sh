1b5d8c80114403d6c08bf97a6405910b6d49e651
Aaron C Spike <aaron@ekips.org>
2011-02-22 20:18:06 -0600

    Revert "gnumake should be properly found now"
    This reverts commit 83ce94de68d5052537915c2f157089b2a12edf9f.

--- do-gcc.sh
+++ do-gcc.sh
@@ -145,6 +145,7 @@ cd "gcc-$GCC_VERSION-build"
 
 if [ $MINGW = 1 ]; then
 	"$(pwd -W)/../gcc-$GCC_VERSION/configure" --prefix="$TARGET_LOCATION" --target=msp430 --enable-languages=c,c++ $WIN32_OPTS --disable-nls --with-pkgversion="MSPGCC4_$VERSION_TAG"
+	GNUMAKE=mingw32-make
 else
 	"$(pwd)/../gcc-$GCC_VERSION/configure" --prefix="$TARGET_LOCATION" --target=msp430 --enable-languages=c,c++ $WIN32_OPTS --with-pkgversion="MSPGCC4_$VERSION_TAG"
 fi
